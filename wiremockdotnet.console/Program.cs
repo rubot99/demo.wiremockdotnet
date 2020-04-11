ce16e15d6d07c1209fec8dd21fa2be93a9239096
reuben.raj <1Rrd0KE9NBWbHVQZU2nT>
2020-04-11 17:22:14 +0100

    Initial commit

---
+++ wiremockdotnet.console/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace wiremockdotnet.console
+{
+    using System;
+    using WireMock.RequestBuilders;
+    using WireMock.ResponseBuilders;
+    using WireMock.Server;
+    using WireMock.Settings;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stub = FluentMockServer.Start(new FluentMockServerSettings
+            {
+                Urls = new[] { "http://+:2001" },
+                StartAdminInterface = true
+            });
+
+
+            stub.Given(
+                    Request.Create()
+                        .WithPath("/mytext/*")
+                        .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/text")
+                    .WithBodyAsJson("result: OK credits: 0"));
+
+            Console.WriteLine("Press any key to stop the server");
+            Console.ReadLine();
+            stub.Stop();
+        }
+    }
+}
